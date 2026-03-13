f8963daf8255c8569307536bab6a9c52d6fbcd0b
diogo <dimcsantos@gmail.com>
2026-03-13 23:08:04 +0000

    rtc.c

--- lab1/rtc.c
+++ lab1/rtc.c
@@ -1,62 +1,58 @@
-#include <minix/syslib.h>
-#include <minix/drivers.h>
 #include "rtc.h"
+#include <minix/syslib.h>
+#include <minix/sysutil.h>
+
+#define TODO return -1
 
 #define RTC_ADDR_REG 0x70
 #define RTC_DATA_REG 0x71
-#define RTC_REG_A    0x0A
-#define RTC_REG_B    0x0B
-#define RTC_REG_DAY  0x07
+#define RTC_REG_A 0x0A
+#define RTC_REG_B 0x0B
+#define RTC_REG_DAY 0x07
 #define RTC_REG_MONTH 0x08
 #define RTC_REG_YEAR 0x09
-#define RTC_UIP_MSK  (1 << 7) 
-#define RTC_DM_MSK   (1 << 2) 
+#define RTC_UIP_MSK (1 << 7)
+#define RTC_DM_MSK (1 << 2)
 
+static int bcd_to_bin(uint8_t bcd) { 
+    return (bcd & 0x0F) + ((bcd >> 4) * 10);
+}
 
-static int bcd_to_bin(uint8_t bcd) {
-    return ((bcd >> 4) * 10) + (bcd & 0x0F);
+static uint8_t rtc_read(uint8_t reg) {
+    uint32_t value;
+
+    sys_outb(RTC_ADDR_REG, reg);
+    sys_inb(RTC_DATA_REG, &value);
+    return (uint8_t)value;
+}
+
+static int is_updating() {
+    uint32_t value;
+
+    sys_outb(RTC_ADDR_REG, RTC_REG_A);
+    sys_inb(RTC_DATA_REG, &value);
+
+    return value & RTC_UIP_MSK;
 }
 
 int rtc_read_date(rtc_date *date) {
-    uint32_t regA = 0;
-    uint32_t regB = 0;
-    uint32_t day, month, year;
+    while(is_updating());
 
-    do {
-    
-        sys_outb(RTC_ADDR_REG, RTC_REG_A);
-        sys_inb(RTC_DATA_REG, &regA);
+    uint8_t day = rtc_read(RTC_REG_DAY);
+    uint8_t month = rtc_read(RTC_REG_MONTH);
+    uint8_t year = rtc_read(RTC_REG_YEAR);
 
-        if (regA & RTC_UIP_MSK) {
-            tickdelay(micros_to_ticks(20000));
-        }
-    } while (regA & RTC_UIP_MSK);
+    uint8_t regB = rtc_read(RTC_REG_B);
 
-
-    
-    sys_outb(RTC_ADDR_REG, RTC_REG_DAY);
-    sys_inb(RTC_DATA_REG, &day);
-
-    sys_outb(RTC_ADDR_REG, RTC_REG_MONTH);
-    sys_inb(RTC_DATA_REG, &month);
-
-    sys_outb(RTC_ADDR_REG, RTC_REG_YEAR);
-    sys_inb(RTC_DATA_REG, &year);
-
-
-    sys_outb(RTC_ADDR_REG, RTC_REG_B);
-    sys_inb(RTC_DATA_REG, &regB);
-
-    
-    if (!(regB & RTC_DM_MSK)) {
-        day = bcd_to_bin((uint8_t)day);
-        month = bcd_to_bin((uint8_t)month);
-        year = bcd_to_bin((uint8_t)year);
+    if (!(regB & 0x04)) {
+        day   = bcd_to_bin(day);
+        month = bcd_to_bin(month);
+        year  = bcd_to_bin(year);
     }
 
-    date->day = (uint8_t) day;
-    date->month = (uint8_t) month;
-    date->year = (uint8_t) year;
+    date->day = day;
+    date->month = month;
+    date->year = year;
 
     return 0;
 }
