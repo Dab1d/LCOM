43df131123c772b3125265e69b5fad1de7de69f7
David Soares Ferreira <up202406798@edu.fe.up.pt>
2026-04-14 02:19:35 +0000

    backup

--- lab3/utils.c
+++ lab3/utils.c
@@ -2,12 +2,19 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <lcom/lcf.h>
+#include "kbc.h"
 
-uint8_t lsb(uint16_t bytes) { return (bytes & 0xFF); }
+uint8_t lsb(uint16_t bytes) {
+    return (bytes & 0xFF);
+}
 
-uint8_t msb(uint16_t bytes) { return ((bytes >> 8) & 0xFF); }
+uint8_t msb(uint16_t bytes) {
+    return ((bytes >> 8) & 0xFF);
+}
 
-bool is_single_byte(uint16_t keycode) { return (keycode <= 0xFF); }
+bool is_single_byte(uint16_t keycode) {
+    return (keycode <= 0xFF);
+}
 
 uint16_t break_from_make(uint16_t keycode) {
     if (is_single_byte(keycode))
