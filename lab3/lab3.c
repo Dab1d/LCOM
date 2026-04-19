34684744d2024d31b6901a9377b9c3c7800d6a29
Leonor Alpoim <up202407742@up.pt>
2026-04-19 16:06:57 +0100

    understanding lab3, beggining

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+uint8_t status; //vai ser para guardar o que vou ler do status register
+//no interrupt handler
+
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
@@ -29,12 +32,29 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int(kbd_test_scan)() {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+//interrupt handler. every time a byte from the keyboard is received
+//kbd_test_scan() should try to assemble a scancode
+void kbc_ih(){
+  util_sys_inb(0x64,&status); //isto lê da porta 0x64 e guarda em status
+
+
+  
 }
+//se nao houver erros no kbc
+//chamo a funçao kbd_print_scancode que vem definida na biblioteca
+
+//esta função não lê o teclado diretamente
+//espera que a kbc_ih lhe entregue os bytes
+int(kbd_test_scan)() {
+  sys_irqsetpolicy(IRQ1, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id);
+  sys_irqrmpolicy(&hook_id);
+  return 0;
+}
+
+//tenho de avisar p sistema que quero receber as interrupções do IRQ1 de forma
+//exclusiva, para que o driver padrão do MINIX não "roube" os bytes antes de mim
+
 
 int(kbd_test_poll)() {
   /* To be completed by the students */
