bfd02adc65a34ed15863b0ceb95142f86e57387d
André Ferreira Baptista <up202403287@edu.fe.up.pt>
2026-04-21 14:59:59 +0100

    Edit timer.c

--- lab2/timer.c
+++ lab2/timer.c
@@ -48,6 +48,7 @@ int(timer_set_frequency)(uint8_t timer, uint32_t freq) {
 
 int(timer_subscribe_int)(uint8_t *bit_no) {
     *bit_no = hook_id;
+    hook_id=0;
     return sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
 }
 
