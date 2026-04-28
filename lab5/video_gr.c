b1fd970996a7e1b7e5d277eaf1215acc6893552d
Andre <andrebap26@gmail.com>
2026-04-28 22:22:30 +0100

    [Lab5] Implement vg_draw_hline() — draw a horizontal line

--- lab5/video_gr.c
+++ lab5/video_gr.c
@@ -13,8 +13,8 @@ int video_test_init(uint16_t mode, uint8_t delay) {
     memset(&r, 0, sizeof(r));
 
     r.intno = 0x10;
-    r.ax = 0x4F02;          // VBE set mode
-    r.bx = mode | BIT(14);  // enable linear framebuffer
+    r.ax = 0x4F02;          //VBE set mode
+    r.bx = mode | BIT(14);  //enable linear framebuffer
 
     if (sys_int86(&r) != OK) {
         printf("Error: sys_int86 failed\n");
@@ -32,7 +32,7 @@ int video_test_init(uint16_t mode, uint8_t delay) {
 
 int vg_draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
 
-    /* verifiva boundaries para evitar crashes */
+    /* verifica boundaries para evitar crashes */
     if (x >= vmi.XResolution || y >= vmi.YResolution) {
         return 1;
     }
@@ -40,8 +40,20 @@ int vg_draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
     /*calcular vram offset */
     unsigned int offset = (y * vmi.XResolution + x) * bytes_per_pixel;
 
-    /* 3. Write color (LSB bytes only) */
+    /* colocar a cor no pixel */
     memcpy(&video_mem[offset], &color, bytes_per_pixel);
+
+    return 0;
+}
+
+int vg_draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
+
+    for (uint16_t i = 0; i < len; i++) {
+
+        if (vg_draw_pixel(x + i, y, color) != 0) {
+            return 1;
+        }
+    }
 
     return 0;
 }
