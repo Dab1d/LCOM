812ec10af1ae25f4da3ccfc489b0e8980ed2eed0
Andre <andrebap26@gmail.com>
2026-04-28 17:15:41 +0100

    [Lab5] Set up lab5 workspace and Makefile

---
+++ lab5/video_gr.c
@@ -0,0 +1,11 @@
+#include "video_gr.h"
+
+int vg_init(uint16_t mode) {
+    
+    return 0;
+}
+
+int vg_exit() {
+    
+    return 0;
+}
---
+++ lab5/video_gr.h
@@ -0,0 +1,9 @@
+#ifndef VIDEO_GR_H
+#define VIDEO_GR_H
+
+#include <lcom/lcf.h>
+
+int vg_init(uint16_t mode);
+int vg_exit();
+
+#endif
