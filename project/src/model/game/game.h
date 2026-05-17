a175ce1cf0eb49fb1a725cda00154d2d6c12743d
Leonor Alpoim <up202407742@up.pt>
2026-05-17 17:20:44 +0100

    started the Model; some doubts tho

---
+++ project/src/model/game/game.h
@@ -0,0 +1,16 @@
+#ifndef __PROJECT_GAME_H
+#define __PROJECT_GAME_H
+
+//depois será preciso dar include dos models que formos
+//criando 
+
+typedef enum {
+    START,
+    PAUSE,
+    GAME,
+    EXIT
+}
+
+//fará sentido ter win ou lose visto que sao dois jogadores?
+
+#endif /* __PROJ_GAME_H */
