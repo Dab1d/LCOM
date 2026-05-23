af8331fe5fd3d888fdef07c2e5e42323e9deabe9
Leonor Alpoim <up202407742@up.pt>
2026-05-23 17:06:58 +0100

    started to develop model for car and obstacleaka beggining of issue #202; next to do -> view of them

--- proj/src/model/game/game.h
+++ proj/src/model/game/game.h
@@ -1,6 +1,7 @@
 #ifndef __PROJ_GAME_H
 #define __PROJ_GAME_H
 
+#include "../sprite/sprite.h"
 //depois será preciso dar include dos models que formos
 //criando 
 
@@ -18,6 +19,15 @@ void game_init(void);
 GameState game_get_state(void);
 void game_set_state( GameState new_state);
 int game_check_collision(void);
+
+typedef struct{
+    Sprite *sprite;
+    int speed;
+} Car;
+
+typedef struct{
+    Sprite *sprite;
+} Obstacle;
 
 #endif /* __PROJ_GAME_H */
 
--- proj/src/model/sprite/sprite.h
+++ proj/src/model/sprite/sprite.h
@@ -1,2 +1,13 @@
+#ifndef __PROJ_SPRITE_H
+#define __PROJ_SPRITE_H
 
+#include <stdint.h>
+
+typedef struct {
+    int x, y;           // posição no ecrã
+    int width, height;  // dimensões
+    uint32_t *pixmap;   // pixels do XPM já convertidos
+} Sprite;
+
+#endif /* __PROJ_SPRITE_H */
 
