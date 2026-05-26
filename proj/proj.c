6266b808f03ed908a8a8e7e4640d27212e5cd9c3
Andre <andrebap26@gmail.com>
2026-05-26 15:36:43 +0100

    element e car model implementados, algumas mudanças no game.c e game.h necessárias após a cração do model

--- proj/proj.c
+++ proj/proj.c
@@ -1,13 +1,17 @@
 #include <lcom/lcf.h>
 #include "src/model/game/game.h"
 
-
-
 int proj_main_loop(int argc, char* argv[]) {
-    uint8_t bit_no;
-    if (timer_set_frequency(0, 60) != 0) return 1;
-    if (timer_subscribe_int(&bit_no) != 0) return 1;
-    uint32_t irq_set = BIT(bit_no);
+    // --- Subscrever interrupções ---
+    uint8_t timer_bit, kbd_bit, mouse_bit;
+    if (timer_set_frequency(0, 60) != 0)       return 1;
+    if (timer_subscribe_int(&timer_bit) != 0)   return 1;
+    if (keyboard_subscribe_int(&kbd_bit) != 0)  return 1;
+    if (mouse_subscribe_int(&mouse_bit) != 0)   return 1;
+
+    uint32_t timer_irq_set    = BIT(timer_bit);
+    uint32_t keyboard_irq_set = BIT(kbd_bit);
+    uint32_t mouse_irq_set    = BIT(mouse_bit);
 
     game_init();
 
@@ -15,49 +19,42 @@ int proj_main_loop(int argc, char* argv[]) {
     message msg;
     int r;
 
-    while (game_get_state() != EXIT) {//como frequencia esta a 60 recebemos 60 interrupts por segundo (60hz)
+    while (game_get_state() != EXIT) {
         if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
             printf("driver_receive failed: %d\n", r);
             continue;
         }
+
         if (is_ipc_notify(ipc_status)) {
             switch (_ENDPOINT_P(msg.m_source)) {
                 case HARDWARE:
-                    if (msg.m_notify.interrupts & irq_set) {
-                        timer_int_handler(); //aumentamos o counter aqui caso seja preciso contar tempo para mais algo 
+                    if (msg.m_notify.interrupts & timer_irq_set) {
+                        timer_int_handler();
                         switch (game_get_state()) {
                             case MAIN_MENU:
-                                if (input_menu_start_pressed())
-                                    game_set_state(GAMEPLAY);
+                                // TODO: game_process_input() + game_render_menu()
                                 break;
                             case GAMEPLAY:
-                                draw_clear(0x333333);  // limpa o ecrã
-                                draw_car(car1);   // jogador 1
-                                draw_car(car2);    // jogador 2
-                                draw_obstacle(obs);     //desenha os obstaculos
-                                if (game_check_collision())
+                                game_process_input();
+                                game_update();
+                                game_process_collisions();
+                                if (game_is_over())
                                     game_set_state(GAME_OVER);
+                                game_render();
                                 break;
                             case PAUSE:
                                 break;
                             case GAME_OVER:
-                                if (input_gameover_restart_pressed())
-                                    game_set_state(GAMEPLAY);
-                                else if (input_gameover_menu_pressed())
-                                    game_set_state(MAIN_MENU);
+                                // TODO: input restart/menu
                                 break;
                             case EXIT:
                                 break;
                         }
                     }
-                    //keyboard - player 1
-                    if (msg.m_notify.interrupts & keyboard_irq_set) {
-                        keyboard_int_handler(); // atualiza estado interno do teclado
-                    }
-                    //mouse - player 2
-                    if (msg.m_notify.interrupts & mouse_irq_set) {
-                        mouse_int_handler(); // atualiza estado interno do rato
-                    }
+                    if (msg.m_notify.interrupts & keyboard_irq_set)
+                        keyboard_int_handler();
+                    if (msg.m_notify.interrupts & mouse_irq_set)
+                        mouse_int_handler();
                     break;
                 default:
                     break;
@@ -65,31 +62,17 @@ int proj_main_loop(int argc, char* argv[]) {
         }
     }
 
-    if (timer_unsubscribe_int() != 0) return 1;
+    timer_unsubscribe_int();
+    keyboard_unsubscribe_int();
+    mouse_unsubscribe_int();
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    // sets the language of LCF messages (can be either EN-US or PT-PT)
     lcf_set_language("EN-US");
-
-    // enables to log function invocations that are being "wrapped" by LCF
-    // [comment this out if you don't want/need it]
     lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
-
-    // enables to save the output of printf function calls on a file
-    // [comment this out if you don't want/need it]
     lcf_log_output("/home/lcom/labs/proj/output.txt");
-
-    // handles control over to LCF
-    // [LCF handles command line arguments and invokes the right function]
-    if (lcf_start(argc, argv))
-        return 1;
-
-    // LCF clean up tasks
-    // [must be the last statement before return]
+    if (lcf_start(argc, argv)) return 1;
     lcf_cleanup();
-
     return 0;
 }
-
---
+++ proj/src/model/car/car.c
@@ -0,0 +1,73 @@
+#include "car.h"
+#include <stdlib.h>
+
+static double lane_to_x(int lane) {
+    return (double)(lane * CAR_LANE_WIDTH);
+}
+
+Car* create_car(int initial_lane, int car_width, int car_height) {
+    Car* car = (Car*) malloc(sizeof(Car));
+    if (car == NULL) return NULL;
+
+    init_element(&car->base, lane_to_x(initial_lane), 0.0, car_width, car_height);
+
+    car->lane           = initial_lane;
+    car->track_progress = 0;
+    car->state          = CAR_STATE_NORMAL;
+
+    return car;
+}
+
+void destroy_car(Car* car) {
+    if (car != NULL) {
+        free(car);
+    }
+}
+
+void car_move_lane(Car* car, int direction) {
+    if (car == NULL || !car->base.is_active) return;
+    if (car->state == CAR_STATE_EXPLODED)    return;
+
+    int new_lane = car->lane + direction;
+    if (new_lane < 0 || new_lane >= TRACK_LANES) return;
+
+    car->lane   = new_lane;
+    car->base.x = lane_to_x(new_lane);
+}
+
+void car_take_damage(Car* car) {
+    if (car == NULL || !car->base.is_active) return;
+
+    switch (car->state) {
+        case CAR_STATE_NORMAL:
+            car->state = CAR_STATE_DAMAGED;
+            break;
+        case CAR_STATE_DAMAGED:
+            car->state = CAR_STATE_BURNING;
+            break;
+        case CAR_STATE_BURNING:
+            car->state          = CAR_STATE_EXPLODED;
+            car->base.is_active = false; // Retira o carro do jogo
+            break;
+        case CAR_STATE_EXPLODED:
+            break;
+    }
+}
+
+void car_apply_boost(Car* car, int tiles) {
+    if (car == NULL || !car->base.is_active) return;
+    if (car->state == CAR_STATE_EXPLODED)    return;
+
+    car->track_progress += tiles;
+}
+
+void reset_car(Car* car, int initial_lane) {
+    if (car == NULL) return;
+
+    car->lane           = initial_lane;
+    car->base.x         = lane_to_x(initial_lane);
+    car->base.y         = 0.0;
+    car->base.is_active = true;
+    car->track_progress = 0;
+    car->state          = CAR_STATE_NORMAL;
+}
