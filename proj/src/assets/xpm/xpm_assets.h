513c9af454de81ee1f1ad54f8afd3ec646b9bc9a
Andre <andrebap26@gmail.com>
2026-06-04 00:54:49 +0100

    restruturação de pastas

--- proj/src/assets/xpm/xpm_assets.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PROJ_XPM_ASSETS_H
-#define __PROJ_XPM_ASSETS_H
-
-/* Declarações dos arrays XPM — definidos ao incluir os ficheiros .xpm */
-extern char *redcar_s1_1_xpm[];
-
-#endif /* __PROJ_XPM_ASSETS_H */
